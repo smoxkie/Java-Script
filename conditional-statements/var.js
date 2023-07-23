998a5d3c81dc1b1768f12286c365f0735a1f9cf3
mishravijaylami2014@gmail.com <mishravijaylami2014@gmail.com>
2023-07-23 22:01:57 +0530

    conditional commit

---
+++ conditional-statements/var.js
@@ -0,0 +1,38 @@
+// var dulha = "dog";
+// var dulhan = "doggy";
+// console.log(dulha + " weds " + dulhan);
+// var dulha = "bloody";
+// console.log(dulha + " weds " + dulhan);
+
+   // Hoisting.....
+   // var a;
+   // console.log(a);
+   //  a = 12;
+
+
+   // for(let i=0;i<=10;i++){
+   //    console.log(i);
+   // }
+
+   //  function abcd(){
+   //    console.log('hello vishwas');
+   //  }
+   //  abcd();
+   //  function abcd(a,b,c,d){
+   //    console.log('hello vishwas');
+   //    console.log(a,b,c,d);
+   //  }
+   //  abcd(2,4,6,8);
+
+
+    let arr = [5,6,7,8];
+   //  console.log(arr[0]);
+   arr[0];
+    console.log(arr[1]);
+    console.log(arr[2]);
+    console.log(arr[3]);
+   // console.log(arr.push(9));
+   console.log(arr.pop());
+   
+
+    
